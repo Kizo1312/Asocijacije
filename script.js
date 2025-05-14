37ce83af16bea0b6698216de1e7578be89a335c2
Stefan <stefanz318@gmail.com>
2025-05-14 19:26:29 +0200

    1% done

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function onClick() {
-  let sunce = document.getElementById("sunce");
-  sunce.innerHTML = "Sunce";
+function onClick(s) {
+  let sunce = document.getElementsByClassName("button");
+  sunce[s].innerHTML = "Sunce";
+}
+function ZaCentar(event) {
+  if (event.key == "Enter") {
+    let tocan = document.getElementById("KURAC");
+    tocan.innerHTML = "nije";
+  }
+}
+function zaMaloRijesenje(event, i) {
+  if (event.key == "Enter") {
+    console.log[i];
+    let tocan = document.getElementsByClassName("maloRijesenje");
+    tocan[i].innerHTML = "nije";
+  }
 }
