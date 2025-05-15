9c6d70e616feac1a7bea77cea9a3cddd36ab7292
Toma Ilcic <toma.ilcic@gmail.com>
2025-05-15 20:00:37 +0200

    gluposti

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-function onClick() {
-  let sunce = document.getElementById("sunce");
-  sunce.innerHTML = "Sunce";
+import { igre } from "./game.js";
+console.log(igre);
+console.log(igre[0].grupaA);
+const letters = ["A", "B", "C", "D"];
+
+const data = {
+  A: igre[0].grupaA,
+  B: igre[0].grupaB,
+  C: igre[0].grupaC,
+  D: igre[0].grupaD,
+};
+
+function reveal(letter, index) {
+  const buttons = document.getElementsByClassName(`button${letter}`);
+  buttons[index].innerHTML = data[letter][index];
+}
+
+letters.forEach((letter) => {
+  document.querySelectorAll(`.button${letter}`).forEach((btn, index) => {
+    btn.addEventListener("click", () => reveal(letter, index));
+  });
+});
+
+function ZaCentar(event) {
+  if (event.key == "Enter") {
+    let tocan = document.getElementById("tocni");
+    tocan.innerHTML = "nije";
+  }
+}
+function zaMaloRijesenje(event, i) {
+  if (event.key == "Enter") {
+    console.log(i);
+    let tocan = document.getElementsByClassName("maloRijesenje");
+    tocan[i].innerHTML = "nije";
+  }
 }
